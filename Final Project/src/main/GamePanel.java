4c853f43097d02d04f293f3179efd2569505a65d
ssuperkid5 <ssuperkid5@yahoo.com>
2017-05-09 09:42:30 -0700

    ah

--- Final Project/src/main/GamePanel.java
+++ Final Project/src/main/GamePanel.java
@@ -1,9 +1,13 @@
 package main;
 
 import javax.swing.JPanel;
-import java.awt.Dimension;
 
-public class GamePanel extends JPanel implements Runnable {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+public class GamePanel extends JPanel implements Runnable, KeyListener{
     /**
      * 
      */
@@ -12,13 +16,56 @@ public class GamePanel extends JPanel implements Runnable {
     // Dimensions
     private static final int WIDTH = 1024;
     private static final int HEIGHT = 768;
+    
+    // Thread
+    private Thread thread;
+    private boolean running;
+    private int FPS = 60;
+    private long targetTime = 1000 / FPS;
+    
+    // Images
+    private BufferedImage image;
+    private Graphics2D graphics;
 
     public void run() {
+	init();
+    }
+
+    private void init() {
+	// TODO Auto-generated method stub
+	image = new BufferedImage(WIDTH, HEIGHT);
     }
 
     public GamePanel() {
 	setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	setFocusable(true);
 	requestFocus();
+    }
+    
+    public void addNotify(){
+	if (thread == null)
+	{
+	    thread = new Thread(this);
+	    addKeyListener(this);
+	    thread.start();
+	}
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
     }
 }
