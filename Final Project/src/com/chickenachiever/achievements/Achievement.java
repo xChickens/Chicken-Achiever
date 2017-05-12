5811496b7939336ab77da396141948940b70d04a
Chris <Chris@Chris-PC>
2017-05-12 01:09:42 -0700

    actual achievement class

--- Final Project/src/com/chickenachiever/achievements/Achievement.java
+++ Final Project/src/com/chickenachiever/achievements/Achievement.java
@@ -1,5 +1,32 @@
 package com.chickenachiever.achievements;
 
 public class Achievement {
+	private String name;
+	private Property[] properties;
+	private boolean unlocked;
 
+	public Achievement(String name, Property[] properties) {
+		this.name = name;
+		this.properties = properties;
+	}
+
+	public String toString() {
+		return null;
+	}
+
+	public void setUnlocked() {
+		unlocked = true;
+	}
+
+	public boolean getUnlocked() {
+		return unlocked;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Property[] getProperties() {
+		return properties;
+	}
 }
