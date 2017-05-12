917e3c84c2ffa74a51226a08f9c2460376a50a2c
ssuperkid5 <ssuperkid5@yahoo.com>
2017-05-12 08:17:07 -0700

    x

---
+++ Final Project/src/com/chickenachiever/model/Launcher.java
@@ -0,0 +1,29 @@
+package com.chickenachiever.model;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import com.chickenachiever.map.TileMap;
+
+public class Launcher extends MapElement{
+
+	private ArrayList<BufferedImage[]> sprites;
+	private final int[] numFrames = { 1 };
+	private static final int IDLE = 0;
+	
+
+	  public Launcher(TileMap tm)
+	  {
+	    super(tm);
+	  }
+	  
+	  public void update()
+	  {
+	  }
+	  
+	  public void draw(Graphics2D g2d)
+	  {
+	  }
+
+}
