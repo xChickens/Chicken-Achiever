002348e29c3527cd5118befe11761798687a76c3
Kevin New <kevin.new3@gmail.com>
2017-05-15 08:38:47 -0700

    yep

--- Final Project/src/com/chickenachiever/model/Animation.java
+++ Final Project/src/com/chickenachiever/model/Animation.java
@@ -14,25 +14,37 @@ public class Animation {
 	}
 
 	public void setFrames(BufferedImage[] frames) {
-
+		this.frames = frames;
+		currentFrame = 0;
+		startTime = System.nanoTime();
+		playedOnce = false;
 	}
 
 	public void setDelay(long d) {
-
+		delay = d;
 	}
 
 	public void update() {
-
+		if(delay == -1) 
+			return;
+		long elapsed = (System.nanoTime() - startTime)/ 1000000;
+		if (elapsed > delay) {
+			currentFrame++;
+			startTime =  System.nanoTime();
+		}
+		if (currentFrame == frames.length){
+			currentFrame = 0;
+			playedOnce = true;
+		}
 	}
 
 	public int getFrame() {
-		return 0;
+		return currentFrame;
 
 	}
 
 	public BufferedImage getImage() {
-		return null;
-
+		return frames[currentFrame];
 	}
 
 	public boolean hasPlayedOnce() {
