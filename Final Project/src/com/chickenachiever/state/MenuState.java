ef9a91e947cfe914cc790e74099f96b133536a3c
ssuperkid5 <ssuperkid5@yahoo.com>
2017-05-15 09:12:36 -0700

    BUTOTN

--- Final Project/src/com/chickenachiever/state/MenuState.java
+++ Final Project/src/com/chickenachiever/state/MenuState.java
@@ -4,36 +4,49 @@ import java.awt.Graphics2D;
 
 import javax.swing.ImageIcon;
 
+import com.chickenachiever.main.GamePanel;
 import com.chickenachiever.map.Background;
+import com.chickenachiever.map.Button;
 
 public class MenuState extends GameState {
-	private Background bg;
-	private int currentChoice = 0;
-	ImageIcon i = new ImageIcon(getClass().getResource("/Backgrounds/Menu.png"));
+    private Background bg;
+    private int currentChoice = 0;
+    private ImageIcon i = new ImageIcon(getClass().getResource("/Backgrounds/Menu.png"));
+    private Button playButton;
+    private Button quitButton;
+    private Button creditsButton;
 
-	public MenuState(GameStateManager gsm) {
-		this.gsm = gsm;
-		try {
-			this.bg = new Background(this.i, 1.0D);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public MenuState(GameStateManager gsm) {
+	this.gsm = gsm;
+	try {
+	    this.bg = new Background(this.i, 1.0D);
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
 
-	public void init() {
-	}
+	playButton = new Button(GamePanel.WIDTH / 2 - 110, 220, 220, 60, "Play");
+	quitButton = new Button(GamePanel.WIDTH / 2 - 110, 310, 220, 60, "Quit");
+	creditsButton = new Button(GamePanel.WIDTH / 2 - 110, 400, 220, 60, "Credits");
 
-	public void update() {
-	}
+    }
 
-	public void draw(Graphics2D g) {
-		this.bg.draw(g);
+    public void init() {
+    }
 
-	}
+    public void update() {
+    }
 
-	public void keyReleased(int k) {
-	}
+    public void draw(Graphics2D g) {
+	bg.draw(g);
+	playButton.draw(g);
+	quitButton.draw(g);
+	creditsButton.draw(g);
 
-	public void keyPressed(int k) {
-	}
+    }
+
+    public void keyReleased(int k) {
+    }
+
+    public void keyPressed(int k) {
+    }
 }
