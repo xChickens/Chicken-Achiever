5caf759cea177213dda73120753ac29c9217d7b5
Kevin New <kevin.new3@gmail.com>
2017-05-25 08:59:33 -0700

    idk how the tilemaps work with the map elements

--- Final Project/src/com/chickenachiever/model/Block.java
+++ Final Project/src/com/chickenachiever/model/Block.java
@@ -11,15 +11,21 @@ public class Block extends MapElement {
 	private ArrayList<BufferedImage[]> sprites;
 	private static final int UNTOUCHED = 0;
 	private static final int TOUCHED = 1;
+	private boolean touched = false;
 
 	public Block(TileMap map) {
 		super(map);
 	}
 
 	public void update() {
+		calculateCorners(x,y);
+		if(topLeft || topRight || bottomLeft || bottomRight){
+			if(touched == false){
+				touched = true;
+			}
+		}
 	}
 
 	public void draw(Graphics2D graph) {
 	}
-
 }
