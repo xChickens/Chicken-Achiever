5a08c13563b203b2f62be48bb39cd1f38f13f602
Chris <Chris@Chris-PC>
2017-06-02 01:35:09 -0700

    Spikes, some of the map

--- Final Project/src/com/chickenachiever/model/DSpike.java
+++ Final Project/src/com/chickenachiever/model/DSpike.java
@@ -1,23 +1,16 @@
 package com.chickenachiever.model;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-
-import com.chickenachiever.map.TileMap;
 import com.chickenachiever.map.TileMapRevamp;
 
 public class DSpike extends MapElementRevamp {
 
-	private ArrayList<BufferedImage[]> sprites;
-	private final int[] numFrames = { 1 };
-	private static final int IDLE = 0;
 	private PlayerRevamp p;
 
 	public DSpike(TileMapRevamp tm, int x, int y) {
 		super(tm, x, y);
 		cwidth = 10;
 		cheight = 10;
+		updateImage("DSpikes.png");
 	}
 
 	public void setPlayer(PlayerRevamp p) {
@@ -28,12 +21,10 @@ public class DSpike extends MapElementRevamp {
 		//System.out.println("hi");
 		if (p != null) {
 			if (intersects(p)) {
+				updateImage("DSpikesTouched.png");
 				p.kill();
 			}
 		}
-	}
-
-	public void draw(Graphics2D g2d) {
-	}
+	}	
 
 }
