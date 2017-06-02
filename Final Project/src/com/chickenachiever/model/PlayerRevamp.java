857e79c921d9e1456585998e2b78036af5d62298
sfChen1029 <steven.fx.chen@gmail.com>
2017-06-02 01:25:31 -0700

    chicken jumps higher

--- Final Project/src/com/chickenachiever/model/PlayerRevamp.java
+++ Final Project/src/com/chickenachiever/model/PlayerRevamp.java
@@ -32,7 +32,7 @@ public class PlayerRevamp extends MapElementRevamp {
 		stopSpeed = 0.4;
 		fallSpeed = 0.15;
 		maxFallSpeed = 4.0;
-		jumpStart = -4.8;
+		jumpStart = -6.5;
 		stopJumpSpeed = 0.3;
 
 		facingRight = true;
